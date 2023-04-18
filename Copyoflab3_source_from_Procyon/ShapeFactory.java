1eb111de58dc620399e487d049d0067ee63913fd
lipkaivan2 <131186268+lipkaivan2@users.noreply.github.com>
2023-04-18 19:52:53 +0300

    Зміна форми
    Була змінена форма та колір фігури, тепер це зірка з сірим градієнтом

--- Copyoflab3_source_from_Procyon/ShapeFactory.java
+++ Copyoflab3_source_from_Procyon/ShapeFactory.java
@@ -25,7 +25,7 @@ public class ShapeFactory
         this.width = 25;
         this.height = 25;
         this.stroke = new BasicStroke(3.0f);
-        switch (shape_type / 10) {
+        switch (3) {
             case 1: {
                 this.shape = createStar(3, new Point(0, 0), this.width / 2.0, this.width / 2.0);
                 break;
@@ -56,7 +56,7 @@ public class ShapeFactory
                 throw new Error("type is nusupported");
             }
         }
-        switch (shape_type % 10) {
+        switch (7) {
             case 1: {
                 this.stroke = new BasicStroke(3.0f);
                 break;
